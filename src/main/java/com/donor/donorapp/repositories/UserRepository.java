dac102fc444d789799ea190a20597b1859c8a6e5
Adit <adit.navle@onelifecapital.in>
2026-03-09 18:53:03 +0530

    feat(auth): Add CustomUserDetailsService

--- src/main/java/com/donor/donorapp/repositories/UserRepository.java
+++ src/main/java/com/donor/donorapp/repositories/UserRepository.java
@@ -4,9 +4,12 @@ import com.donor.donorapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface UserRepository extends
         JpaRepository<User, Long>,
         JpaSpecificationExecutor<User> {
 
     boolean existsByEmailIgnoreCase(String email);
+    Optional<User> findByEmail(String email);
 }
